e9332c18c7908b88d9df95e30e059a4049b662e0
Girish V <girish.v@needl.ai>
2020-08-22 15:56:11 +0530

    added app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (MaterialApp(
-      home: Text(
-        'Hello',
-        style: TextStyle(color: Colors.white),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('My First App'),
+        ),
+        body: Text("hello"),
       ),
     ));
   }
