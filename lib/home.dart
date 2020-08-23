effdbcd85b5def95e6a0e33def70144698989c1f
Girish V <girish.v@needl.ai>
2020-08-23 17:51:01 +0530

    init basic layout

---
+++ lib/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/transaction.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = Uuid();
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _transactions = [
+    Transaction(
+      id: uuid.v4(),
+      title: 'Shirt',
+      spent: 19.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: uuid.v4(),
+      title: 'Watch',
+      spent: 39.99,
+      date: DateTime.now().add(
+        Duration(days: -1),
+      ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Text(
+                  'header charts',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Text(
+                  'list of txns',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
