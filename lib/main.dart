f61fc46e436b72f4ac39f452ebdf95ef3d40ca9a
Girish V <girish.v@needl.ai>
2020-08-26 23:10:52 +0530

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_complete_guide/bloc/timer_bloc.dart';
+import 'package:flutter_complete_guide/ticker.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,9 +15,124 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter'),
         ),
         body: Center(
-          child: Text('Flutter'),
+          child: BlocProvider(
+            create: (context) => TimerBloc(ticker: Ticker()),
+            child: Timer(),
+          ),
         ),
       ),
     );
+  }
+}
+
+class Timer extends StatelessWidget {
+  static const TextStyle timerTextStyle = TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: BlocBuilder<TimerBloc, TimerState>(
+              builder: (context, state) {
+                final String minutesStr = ((state.duration / 60) % 60)
+                    .floor()
+                    .toString()
+                    .padLeft(2, '0');
+                final String secondsStr =
+                    (state.duration % 60).floor().toString().padLeft(2, '0');
+                return Text(
+                  '$minutesStr:$secondsStr',
+                  style: Timer.timerTextStyle,
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          BlocBuilder<TimerBloc, TimerState>(
+            buildWhen: (previousState, state) =>
+                state.runtimeType != previousState.runtimeType,
+            builder: (context, state) => Actions(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Actions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: _mapStateToActions(
+        timerBloc: BlocProvider.of<TimerBloc>(context),
+      ),
+    );
+  }
+
+  List<Widget> _mapStateToActions({TimerBloc timerBloc}) {
+    final TimerState state = timerBloc.state;
+
+    if (state is TimerInitial) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.add(
+            TimerStarted(duraiton: 60),
+          ),
+        )
+      ];
+    } else if (state is TimerRunInProgress) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.pause),
+          onPressed: () => timerBloc.add(
+            TimerPaused(),
+          ),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.add(
+            TimerReset(),
+          ),
+        )
+      ];
+    } else if (state is TimerRunPause) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.add(
+            TimerResumed(),
+          ),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.add(
+            TimerReset(),
+          ),
+        )
+      ];
+    } else if (state is TimerRunComplete) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.add(
+            TimerStarted(
+              duraiton: 60,
+            ),
+          ),
+        ),
+      ];
+    }
+    return [];
   }
 }
