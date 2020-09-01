22bfcecfc29d1717cdb9ebc7114a51bd2920bcfa
Girish V <girish.v@needl.ai>
2020-09-01 23:01:29 +0530

    chart bar widget

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String day;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  const ChartBar(
+      {Key key,
+      @required this.day,
+      @required this.spendingAmount,
+      @required this.spendingPctOfTotal})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      fit: FlexFit.tight,
+      child: Column(
+        children: [
+          FittedBox(
+            fit: BoxFit.contain,
+            child: Text(
+              "\$${spendingAmount.toStringAsFixed(0)}",
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Container(
+            height: 100,
+            width: 8,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                  ),
+                ),
+                FractionallySizedBox(
+                  heightFactor: spendingPctOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            day,
+          ),
+        ],
+      ),
+    );
+  }
+}
