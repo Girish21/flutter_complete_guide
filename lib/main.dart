8f90239d7ec7b947294964dfdb838f13745170e3
Girish V <girish.v@needl.ai>
2020-09-03 22:13:45 +0530

    playground init

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,46 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter'),
         ),
         body: Center(
-          child: Text('Flutter'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Flexible(
+                flex: 3,
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                  ),
+                  child: Text(
+                    "test -1 A longg text",
+                  ),
+                ),
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text(
+                    "test - 2",
+                  ),
+                ),
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                  ),
+                  child: Text("test - 2"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
