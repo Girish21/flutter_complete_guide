18e44236e03ca98f4c8fc99443b79b14ac3f9044
Girish V <girish.v@needl.ai>
2020-09-06 18:07:51 +0530

    added meal data

---
+++ lib/models/meal.dart
@@ -0,0 +1,43 @@
+enum Affordability {
+  Affordable,
+  Pricey,
+  Luxurious,
+}
+
+enum Complexity {
+  Simple,
+  Challenging,
+  Hard,
+}
+
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imageUrl;
+  final Affordability affordability;
+  final Complexity complexity;
+  final int duration;
+  final List<String> ingredients;
+  final List<String> steps;
+  final bool isGlutenFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final bool isLactoseFree;
+
+  const Meal({
+    this.id,
+    this.categories,
+    this.title,
+    this.imageUrl,
+    this.affordability,
+    this.complexity,
+    this.duration,
+    this.ingredients,
+    this.steps,
+    this.isGlutenFree,
+    this.isVegan,
+    this.isVegetarian,
+    this.isLactoseFree,
+  });
+}
