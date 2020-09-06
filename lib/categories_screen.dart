a8b9bd29badbec1a99cafd0a76c9c665a035fe3e
Girish V <girish.v@needl.ai>
2020-09-06 16:40:28 +0530

    category grid view

---
+++ lib/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_tile.dart';
+import './dummy_data.dart';
+
+class Categories extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final medai = MediaQuery.of(context);
+    return GridView.builder(
+      itemCount: DUMMY_CATEGORIES.length,
+      padding: const EdgeInsets.only(
+        top: 16,
+        left: 16,
+        right: 16,
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: medai.size.width * 0.05,
+        mainAxisSpacing: medai.orientation == Orientation.portrait
+            ? medai.size.height * 0.02
+            : medai.size.height * 0.05,
+      ),
+      itemBuilder: (context, index) => CategoryTile(
+        color: DUMMY_CATEGORIES[index].color,
+        title: DUMMY_CATEGORIES[index].title,
+      ),
+    );
+  }
+}
---
+++ lib/category_tile.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CategoryTile extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  const CategoryTile({
+    Key key,
+    @required this.title,
+    @required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Center(
+        child: Text(
+          title,
+        ),
+      ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(
+          25,
+        ),
+      ),
+    );
+  }
+}
