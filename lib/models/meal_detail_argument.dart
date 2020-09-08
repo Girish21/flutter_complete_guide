14f4fccf1a2009e9157e62caa5b57ccb72ee7191
Girish V <girish.v@needl.ai>
2020-09-08 23:04:00 +0530

    fixed model

--- lib/models/meal_detail_argument.dart
+++ lib/models/meal_detail_argument.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MealDetailArgument {
-  final String category;
   final String id;
 
   MealDetailArgument({
-    @required this.category,
     @required this.id,
   });
 }
