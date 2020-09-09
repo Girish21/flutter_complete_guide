1f689a59a7e1b42cae5dc12adedc0ce6e7ba0f27
Girish V <girish.v@needl.ai>
2020-09-09 22:23:30 +0530

    remove redundant scaffold

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -8,30 +8,25 @@ class Categories extends StatelessWidget {
   Widget build(BuildContext context) {
     final medai = MediaQuery.of(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
+    return GridView.builder(
+      itemCount: DUMMY_CATEGORIES.length,
+      padding: const EdgeInsets.only(
+        top: 16,
+        left: 16,
+        right: 16,
       ),
-      body: GridView.builder(
-        itemCount: DUMMY_CATEGORIES.length,
-        padding: const EdgeInsets.only(
-          top: 16,
-          left: 16,
-          right: 16,
-        ),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: medai.size.width * 0.05,
-          mainAxisSpacing: medai.orientation == Orientation.portrait
-              ? medai.size.height * 0.02
-              : medai.size.height * 0.05,
-        ),
-        itemBuilder: (context, index) => CategoryTile(
-          color: DUMMY_CATEGORIES[index].color,
-          title: DUMMY_CATEGORIES[index].title,
-          id: DUMMY_CATEGORIES[index].id,
-        ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: medai.size.width * 0.05,
+        mainAxisSpacing: medai.orientation == Orientation.portrait
+            ? medai.size.height * 0.02
+            : medai.size.height * 0.05,
+      ),
+      itemBuilder: (context, index) => CategoryTile(
+        color: DUMMY_CATEGORIES[index].color,
+        title: DUMMY_CATEGORIES[index].title,
+        id: DUMMY_CATEGORIES[index].id,
       ),
     );
   }
