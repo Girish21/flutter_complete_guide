5bf7e60a9234ae8c7f28aeea2469b343b4f4422f
Girish V <girish.v@needl.ai>
2020-09-10 22:48:03 +0530

    added filters

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class Filters extends StatelessWidget {
+class Filters extends StatefulWidget {
   static const FiltersRoute = '/filters';
 
   const Filters({Key key}) : super(key: key);
+
+  @override
+  _FiltersState createState() => _FiltersState();
+}
+
+class _FiltersState extends State<Filters> {
+  var _glutenFree = false;
+  var _vegitarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitchTile({
+    @required Widget title,
+    @required bool value,
+    @required Function changeHadler,
+  }) {
+    return SwitchListTile(
+      title: title,
+      value: value,
+      onChanged: changeHadler,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +37,60 @@ class Filters extends StatelessWidget {
         ),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text(
-          'Filters',
-        ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(
+              24,
+            ),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchTile(
+                  title: Text('Glutten Free'),
+                  value: _glutenFree,
+                  changeHadler: (val) {
+                    setState(() {
+                      _glutenFree = val;
+                    });
+                  },
+                ),
+                _buildSwitchTile(
+                  title: Text('Vegitarian'),
+                  value: _vegitarian,
+                  changeHadler: (val) {
+                    setState(() {
+                      _vegitarian = val;
+                    });
+                  },
+                ),
+                _buildSwitchTile(
+                  title: Text('Vegan'),
+                  value: _vegan,
+                  changeHadler: (val) {
+                    setState(() {
+                      _vegan = val;
+                    });
+                  },
+                ),
+                _buildSwitchTile(
+                  title: Text('Lactose Free'),
+                  value: _lactoseFree,
+                  changeHadler: (val) {
+                    setState(() {
+                      _lactoseFree = val;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
