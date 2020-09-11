dc3615ceeb14a3f89d2902a0fd16260d8023b3b1
Girish V <girish.v@needl.ai>
2020-09-11 23:27:50 +0530

    show favorites

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/meal_detail_argument.dart';
+import 'package:flutter_complete_guide/screens/meals_detail_screen.dart';
+import '../widgets/meals_list.dart';
+import '../models/meal.dart';
 
 class Favorites extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Favorites',
+  final List<Meal> favorites;
+
+  const Favorites({
+    Key key,
+    @required this.favorites,
+  }) : super(key: key);
+
+  void clickHandler(
+    BuildContext context,
+    String id,
+  ) {
+    Navigator.of(context).pushNamed(
+      MealDetail.MealDetailRoute,
+      arguments: MealDetailArgument(
+        id: id,
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return favorites.isEmpty
+        ? Center(
+            child: Text(
+              'Favorites',
+            ),
+          )
+        : MealsList(
+            clickHandler: clickHandler,
+            filteredCategoryMeals: favorites,
+          );
   }
 }
