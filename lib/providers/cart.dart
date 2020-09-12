d0943a9519edb157c77b49644fc7181e5ed69cbe
Girish V <girish.v@needl.ai>
2020-09-12 16:53:47 +0530

    cart provider

---
+++ lib/providers/cart.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    this.id,
+    this.title,
+    this.quantity,
+    this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem({
+    String id,
+    double price,
+    String title,
+  }) {
+    if (_items.containsKey(id)) {
+      _items.update(
+        id,
+        (value) => CartItem(
+          id: value.id,
+          price: value.price,
+          quantity: value.quantity + 1,
+          title: value.title,
+        ),
+      );
+    } else
+      _items.putIfAbsent(
+        id,
+        () => CartItem(
+          id: uuid.v4(),
+          price: price,
+          title: title,
+          quantity: 1,
+        ),
+      );
+  }
+}
