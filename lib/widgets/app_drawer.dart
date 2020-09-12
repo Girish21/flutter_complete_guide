dffd99856f7f4f6eb3a613128d32678e55d2ee05
Girish V <girish.v@needl.ai>
2020-09-12 22:32:11 +0530

    added drawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../screens/orders_screen.dart';
+
+class AppDrawer extends StatelessWidget {
+  Widget buildListTile(Icon icon, String title, Function clickHandler) {
+    return ListTile(
+      leading: icon,
+      title: Text(
+        title,
+      ),
+      onTap: clickHandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text(
+              'Hello Friend!',
+            ),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          buildListTile(
+            Icon(
+              Icons.shop,
+            ),
+            'Shop',
+            () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          buildListTile(
+            Icon(
+              Icons.payment,
+            ),
+            'Orders',
+            () {
+              Navigator.of(context).pushReplacementNamed(
+                OrdersScreen.RouteName,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
