4aeed9076f0e556d3fa857bc4da148ff24acd48a
Girish V <girish.v@needl.ai>
2020-09-12 11:28:51 +0530

    added rounded border

--- lib/widgets/Product_card.dart
+++ lib/widgets/Product_card.dart
@@ -14,30 +14,35 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imgUrl,
-        fit: BoxFit.contain,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(
+        16,
       ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black45,
-        leading: IconButton(
-          icon: Icon(
-            Icons.favorite,
-          ),
-          onPressed: () {},
-          color: Theme.of(context).accentColor,
+      child: GridTile(
+        child: Image.network(
+          imgUrl,
+          fit: BoxFit.contain,
         ),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          icon: Icon(
-            Icons.shopping_cart,
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              Icons.favorite,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
           ),
-          onPressed: () {},
-          color: Theme.of(context).accentColor,
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
         ),
       ),
     );
