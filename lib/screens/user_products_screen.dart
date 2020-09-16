c4270284e2ad96361870d36a6bd2147f4087e762
Girish V <girish.v@needl.ai>
2020-09-16 22:28:45 +0530

    add pull to refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -31,27 +31,31 @@ class UserProducts extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(
-          8,
-        ),
-        child: ListView.separated(
-          separatorBuilder: (context, index) {
-            final products = _products.products;
+      body: RefreshIndicator(
+        onRefresh: () =>
+            Provider.of<Products>(context, listen: false).fetchProducts(),
+        child: Padding(
+          padding: const EdgeInsets.all(
+            8,
+          ),
+          child: ListView.separated(
+            separatorBuilder: (context, index) {
+              final products = _products.products;
 
-            if (index < products.length - 1) return Divider();
-            return null;
-          },
-          itemCount: _products.products.length,
-          itemBuilder: (context, index) {
-            final product = _products.products[index];
+              if (index < products.length - 1) return Divider();
+              return null;
+            },
+            itemCount: _products.products.length,
+            itemBuilder: (context, index) {
+              final product = _products.products[index];
 
-            return UserProductItem(
-              id: product.id,
-              title: product.title,
-              imageUrl: product.imageUrl,
-            );
-          },
+              return UserProductItem(
+                id: product.id,
+                title: product.title,
+                imageUrl: product.imageUrl,
+              );
+            },
+          ),
         ),
       ),
     );
