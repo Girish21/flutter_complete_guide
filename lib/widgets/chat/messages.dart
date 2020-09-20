6aff226fddd518a75388b301d0682a2cb27f44d6
Girish V <girish.v@needl.ai>
2020-09-20 22:11:19 +0530

    refactor to get userid

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,30 +1,43 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/chat/message_chat_bubble.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance
-          .collection('chat')
-          .orderBy('createdAt', descending: true)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting)
+    return FutureBuilder<FirebaseUser>(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (context, auth) {
+        if (auth.connectionState == ConnectionState.waiting)
           return Center(
             child: CircularProgressIndicator(),
           );
+        return StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance
+              .collection('chat')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
 
-        return ListView.builder(
-          itemCount: snapshot.data.documents.length,
-          reverse: true,
-          itemBuilder: (context, index) {
-            final document = snapshot.data.documents[index];
+            return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              reverse: true,
+              itemBuilder: (context, index) {
+                final document = snapshot.data.documents[index];
 
-            return Container(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('${document['text']}'),
-              alignment: Alignment.centerRight,
+                return MessageChatBubble(
+                  message: '${document['text']}',
+                  isMe: document['userId'] == auth.data.uid,
+                  key: ValueKey(
+                    document.documentID,
+                  ),
+                );
+              },
             );
           },
         );
