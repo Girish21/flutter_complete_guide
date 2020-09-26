02747e439e21297363ef4ebf44586814ea7761a0
Girish V <girish.v@needl.ai>
2020-09-26 17:05:45 +0530

    handle notifications

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/chat/message_input.dart';
 import '../widgets/chat/messages.dart';
 
-class ChatScreeen extends StatelessWidget {
+class ChatScreeen extends StatefulWidget {
+  @override
+  _ChatScreeenState createState() => _ChatScreeenState();
+}
+
+class _ChatScreeenState extends State<ChatScreeen> {
+  @override
+  void initState() {
+    super.initState();
+
+    var fm = FirebaseMessaging();
+    fm.requestNotificationPermissions();
+    fm.configure(
+      onMessage: (message) {
+        print(message);
+        return Future.value(true);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
